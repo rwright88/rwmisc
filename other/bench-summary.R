3e384766d30c983e2c7d87cd20009617eebe4f79
Ray Wright <raywright@pa.gov>
2019-03-14 16:39:00 -0400

    Benchmark test varies by number of rows and cols

--- other/bench-summary.R
+++ other/bench-summary.R
@@ -6,6 +6,7 @@ library(rwmisc)
 library(microbenchmark)
 
 n_rows <- 10^(3:6)
+n_cols <- c(4, 20, 100)
 
 # funs --------------------------------------------------------------------
 
@@ -24,18 +25,17 @@ f_rwmisc <- function(data) {
 create_data <- function(n_rows, reps) {
   dat <- map_dfc(seq_len(reps), function(.x) {
     tibble(
-      logical = sample(c(TRUE, FALSE, NA), n_rows, replace = TRUE),
-      integer = sample(c(1:10, NA), n_rows, replace = TRUE),
-      double = rnorm(n_rows, mean = 5, sd = 1),
-      character = sample(c(letters[1:10], NA), n_rows, replace = TRUE)
+      lgl = sample(c(TRUE, FALSE, NA), n_rows, replace = TRUE),
+      int = sample(c(1:10, NA), n_rows, replace = TRUE),
+      dbl = rnorm(n_rows, mean = 5, sd = 1),
+      chr = sample(c(letters[1:10], NA), n_rows, replace = TRUE)
     )
   })
 
   dat
 }
 
-bench_funs <- function(f_base, f_skimr, f_rwmisc, n_rows) {
-  n_cols <- 100
+bench_funs <- function(f_base, f_skimr, f_rwmisc, n_rows, n_cols) {
   reps <- round(n_cols / 4)
   data <- create_data(n_rows = n_rows, reps = reps)
 
@@ -43,20 +43,23 @@ bench_funs <- function(f_base, f_skimr, f_rwmisc, n_rows) {
     f_base(data),
     f_skimr(data),
     f_rwmisc(data),
-    times = 5
+    times = 10
   )
 
-  res <- mutate(res, n_rows, time = time / 1e9)
+  res <- mutate(res, n_rows, n_cols, time = time / 1e9)
   res
 }
 
-run_benches <- function(f_base, f_skimr, f_rwmisc, n_rows) {
-  res <- lapply(n_rows, function(.x) {
+run_benches <- function(f_base, f_skimr, f_rwmisc, n_rows, n_cols) {
+  params <- crossing(n_rows, n_cols)
+
+  res <- pmap(params, function(n_rows, n_cols) {
     bench_funs(
       f_base = f_base,
       f_skimr = f_skimr,
       f_rwmisc = f_rwmisc,
-      n_rows = .x
+      n_rows = n_rows,
+      n_cols = n_cols
     )
   })
 
@@ -64,14 +67,18 @@ run_benches <- function(f_base, f_skimr, f_rwmisc, n_rows) {
   res
 }
 
-plot_medians <- function(data) {
+plot_medians <- function(data, x, facet) {
+  x <- sym(x)
+  facet <- sym(facet)
+
   data %>%
-    group_by(n_rows, expr) %>%
+    group_by(!!x, !!facet, expr) %>%
     summarise(time_p50 = median(time)) %>%
     ungroup() %>%
-    ggplot(aes(n_rows, time_p50, color = expr)) +
+    ggplot(aes(!!x, time_p50, color = expr)) +
     geom_point(size = 2) +
-    geom_line(size = 1.1) +
+    geom_line(size = 1) +
+    facet_wrap(facet) +
     scale_x_log10(breaks = 10 ^ (-10:10), minor_breaks = NULL) +
     scale_y_log10(breaks = 10 ^ (-10:10), minor_breaks = NULL) +
     scale_color_brewer(type = "qual", palette = "Set1") +
@@ -81,9 +88,15 @@ plot_medians <- function(data) {
 
 # run ---------------------------------------------------------------------
 
-dat <- create_data(n_rows = 1e5, reps = 2)
+dat <- create_data(n_rows = 1e6, reps = 1)
 summary2(dat)
 
-res <- run_benches(f_base, f_skimr, f_rwmisc, n_rows = n_rows)
+res <- run_benches(
+  f_base = f_base,
+  f_skimr = f_skimr,
+  f_rwmisc = f_rwmisc,
+  n_rows = n_rows,
+  n_cols = n_cols
+)
 
-plot_medians(res)
+plot_medians(res, x = "n_rows", facet = "n_cols")
