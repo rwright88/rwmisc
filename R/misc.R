db0b69545a4a935487befb5e06d10e12d4b5fe2c
Ray Wright <raywright@pa.gov>
2019-03-15 16:07:02 -0400

    Allow additional args to identical()

--- R/misc.R
+++ R/misc.R
@@ -1,14 +1,15 @@
 #' Test objects in list for exact equality
 #'
 #' @param x List of objects
+#' @param ... Additional arguments passed on to `identical()`
 #' @return A single logical value.
 #' @export
-identical_all <- function(x) {
+identical_all <- function(x, ...) {
   if (!is.list(x) || length(x) < 2) {
     stop("`x` must be a list of at least length 2.", call. = FALSE)
   }
 
   all(vapply(x[-1], FUN.VALUE = logical(1), FUN = function(.x) {
-    identical(.x, x[[1]])
+    identical(.x, x[[1]], ...)
   }))
 }
