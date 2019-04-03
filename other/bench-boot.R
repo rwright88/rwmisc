385ae9e70f60ff968eefea6326eb837c6ca16850
Ray Wright <raywright88@gmail.com>
2019-04-03 07:18:41 -0400

    Compare benchmark

--- other/bench-boot.R
+++ other/bench-boot.R
@@ -4,26 +4,51 @@ library(dplyr)
 library(bench)
 library(rwmisc)
 
-x <- runif(1e4)
+x <- runif(1e3)
 times <- 1e4
 
-f1 <- function(x, times) {
+boot_ci_mat <- function(x, times, type = c("continuous", "discrete"), probs = c(0.1, 0.5, 0.9), w = 1) {
+  stopifnot(is.numeric(times), length(times) == 1)
+  stopifnot(is.numeric(probs), all(probs >= 0 & probs <= 1))
+  type <- match.arg(type)
+
   x_len <- length(x)
+  w_len <- length(w)
+
+  if (w_len != 1 & x_len != w_len) {
+    stop("`x` and `w` must have the same length.", call. = FALSE)
+  }
+  if (w_len == 1) {
+    w <- rep(1, x_len)
+  } else {
+    w <- w / mean(w)
+  }
+
   reps <- seq_len(times)
   temp <- vector(typeof(x), x_len)
 
-  vapply(reps, FUN.VALUE = temp, FUN = function(rep) {
-    sample(x, size = x_len, replace = TRUE)
-  })
+  size <- x_len * times
+
+  xs <- matrix(sample(x, size = size, replace = TRUE), nrow = x_len, ncol = times)
+
+  if (type == "continuous") {
+    means <- colMeans(xs)
+    out <- quantile(means, probs = probs, names = FALSE)
+    out <- setNames(out, paste0("p", probs * 100))
+  } else if (type == "discrete") {
+    vals <- sort(unique(x))
+    out <- lapply(vals, function(val) {
+      means <- colMeans(xs == val)
+      res <- quantile(means, probs = probs, names = FALSE)
+      setNames(res, paste0("p", probs * 100))
+    })
+    out <- setNames(out, vals)
+  }
+
+  out
 }
 
-f2 <- function(x, times) {
-  x_len <- length(x)
-
-  matrix()
-}
-
-bench::mark(check = FALSE,
-  f1(x, times),
-  f2(x, times)
+bench::mark(check = FALSE, iterations = 10,
+  boot_ci(x, times),
+  boot_ci_mat(x, times)
 )
