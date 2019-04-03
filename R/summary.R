cef940763788aba974c7d924cd0946cfd95d110b
Ray Wright <raywright@pa.gov>
2019-04-03 09:26:20 -0400

    Move print function method

--- R/summary.R
+++ R/summary.R
@@ -148,3 +148,18 @@ shorten_type <- function(x) {
   x[x == "complex"]   <- "cpl"
   x
 }
+
+#' Print summary2
+#'
+#' @param x Data frame
+#' @param digits Minimum number of significant digits to use
+#' @param n_rows Maximum number of rows to print
+#' @param ... Other arguments passed on to print
+#' @export
+print.summary2 <- function(x, digits = 4, n_rows = 10, ...) {
+  stopifnot(is.numeric(digits), length(digits) == 1)
+  stopifnot(is.numeric(n_rows), length(n_rows) == 1)
+  n_cells <- round(ncol(x) * n_rows)
+  print.data.frame(x, digits = digits, max = n_cells)
+  invisible(x)
+}
