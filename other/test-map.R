ade99ae44c3b00283a19a52f8fddc13d03c363e2
Ray Wright <raywright@pa.gov>
2019-04-30 09:35:50 -0400

    Update script

--- other/test-map.R
+++ other/test-map.R
@@ -51,16 +51,34 @@ metro <- get_metro(county)
 county <- calc_changes(county, bys = "county", first = year_first, last = year_last)
 metro <- calc_changes(metro, bys = c("cbsa_code", "cbsa_name"), first = year_first, last = year_last)
 
+states <- unique(str_sub(county$county, 1, 2))
+states <- states[!(states %in% c("02", "15", "72"))]
+
+county %>%
+  rename(county_fips = county) %>%
+  mutate(change_aa = change_aa * 100) %>%
+  mutate(change_aa = case_when(
+    change_aa < -2 ~ -2,
+    change_aa > 2 ~ 2,
+    TRUE ~ change_aa
+  )) %>%
+  rwmisc::map_us_county(fill = "change_aa", type = "centroids", size = "pop", state = states) +
+  scale_fill_gradientn(colors = RColorBrewer::brewer.pal(9, "RdBu")) +
+  scale_size_continuous(range = c(0.5, 15), guide = FALSE) +
+  rwmisc::theme_rw()
+
+ggsave("~/map-county.png", dpi = 300, width = 14, height = 8)
+
 metro %>%
   mutate(change_aa = change_aa * 100) %>%
   mutate(change_aa = case_when(
-    change_aa < -2.5 ~ -2.5,
-    change_aa > 2.5 ~ 2.5,
+    change_aa < -2 ~ -2,
+    change_aa > 2 ~ 2,
     TRUE ~ change_aa
   )) %>%
   rwmisc::map_us_metro(fill = "change_aa", size = "pop") +
-  scale_fill_gradientn(colors = RColorBrewer::brewer.pal(9, "RdPu")) +
+  scale_fill_gradientn(colors = RColorBrewer::brewer.pal(9, "RdBu")) +
   scale_size_continuous(range = c(0.5, 20), guide = FALSE) +
   rwmisc::theme_rw()
 
-ggsave("~/map.png", dpi = 300, width = 14, height = 8)
+ggsave("~/map-metro.png", dpi = 300, width = 14, height = 8)
