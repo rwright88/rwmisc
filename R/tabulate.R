592e7f83364ebedf91c199529866ed905fd535fc
Ray Wright <raywright@pa.gov>
2019-05-21 15:22:14 -0400

    Add alternative to tabulate function

---
+++ R/tabulate.R
@@ -0,0 +1,41 @@
+#' Alternative to tabulate
+#'
+#' @param x Numeric vector
+#' @return Numeric vector of counts of unique values of x
+#' @export
+tabulate2 <- function(x) {
+  stopifnot(is.numeric(x))
+  if (typeof(x) != "integer") {
+    x <- as.integer(x)
+  }
+
+  nas <- sum(is.na(x))
+  zeros <- sum(x == 0, na.rm = TRUE)
+
+  x_min <- min(x, na.rm = TRUE)
+  x_max <- max(x, na.rm = TRUE)
+
+  if (x_min < 0) {
+    neg <- rev(tabulate(abs(x[x < 0])))
+  } else {
+    neg <- NULL
+  }
+
+  if (zeros == 0) {
+    zeros <- NULL
+  }
+
+  if (x_max > 0) {
+    pos <- tabulate(x)
+  } else {
+    pos <- NULL
+  }
+
+  nms <- seq.int(x_min, x_max)
+  nms <- c(NA, nms)
+
+  out <- c(nas, neg, zeros, pos)
+  out <- stats::setNames(out, nms)
+  out <- out[out != 0]
+  out
+}
---
+++ other/bench-tabulate.R
@@ -0,0 +1,69 @@
+# benchmark tabulate2
+
+library(tidyverse)
+library(rwmisc)
+library(vctrs)
+library(bench)
+
+sizes <- 10^(4:6)
+rates <- 10^(-5:-1)
+
+# funs --------------------------------------------------------------------
+
+run_bench <- function(sizes, rates) {
+  params <- tidyr::crossing(
+    size = sizes,
+    rate = rates
+  )
+  params <- filter(params, size * rate > 5)
+
+  out <- lapply(seq_len(nrow(params)), function(i) {
+    size <- params$size[[i]]
+    rate <- params$rate[[i]]
+
+    n_unique <- round(size * rate)
+    vals <- seq(-n_unique / 2, n_unique / 2)
+    x <- sample(vals, size = size, replace = TRUE)
+
+    res <- bench::mark(
+      sum(x),
+      tabulate(x),
+      tabulate2(x),
+      vec_count(x),
+      table(x),
+      check = FALSE,
+      iterations = 10
+    )
+
+    res <- res[, c("expression", "median", "n_itr")]
+    res$median <- as.numeric(res$median)
+    res$size <- size
+    res$rate <- rate
+    res
+  })
+
+  out <- bind_rows(out)
+  out
+}
+
+plot_bench <- function(data, x, facet) {
+  x_ <- sym(x)
+
+  data %>%
+    mutate(expression = reorder(expression, desc(median))) %>%
+    ggplot(aes(!!x_, median, color = expression)) +
+    geom_point(size = 2) +
+    geom_line(size = 1) +
+    facet_wrap(facet, nrow = 1) +
+    scale_x_log10(breaks = 10 ^ (-10:10), minor_breaks = NULL) +
+    scale_y_log10(breaks = 10 ^ (-10:10), minor_breaks = NULL) +
+    scale_color_brewer(type = "qual", palette = "Set1") +
+    annotation_logticks() +
+    theme_rw()
+}
+
+# run ---------------------------------------------------------------------
+
+res <- run_bench(sizes = sizes, rates = rates)
+
+plot_bench(res, x = "rate", facet = "size")
