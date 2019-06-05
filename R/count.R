1dd10c28c64cf9970cfb5048fa9d49db07609b9e
Ray Wright <raywright@pa.gov>
2019-06-05 11:59:03 -0400

    Only need to use tabulate if factor

--- R/count.R
+++ R/count.R
@@ -4,8 +4,13 @@
 #' @return List of unique values of x and counts of each unique value
 #' @export
 count <- function(x) {
-  keys <- sort(unique(x), na.last = FALSE)
-  counts <- tabulate(fastmatch::fmatch(x, keys))
-  keys <- as.vector(keys)
+  if (is.factor(x)) {
+    keys <- levels(x)
+    counts <- tabulate(x)
+  } else {
+    keys <- sort(unique(x), na.last = FALSE)
+    counts <- tabulate(fastmatch::fmatch(x, keys))
+    keys <- as.vector(keys)
+  }
   list(key = keys, count = counts)
 }
